6dc38ad490452ac3041f8ae1ecbc96c8cb5a447b
anjali2612 <61150678+anjali2612@users.noreply.github.com>
2022-01-22 13:44:52 +0530

    Add files via upload

---
+++ Userr.js
@@ -0,0 +1,10 @@
+$('#mybutton').click(function() {
+    console.log('search icon clicked!');
+  });
+
+
+  var products = document.getElementById("cartItemsList");
+  cartItemsList.innerHTML = "<li>" + product + "</li>";
+
+
+  
---
+++ table.js
@@ -0,0 +1,57 @@
+$("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
+
+$(document).ready(function() {
+
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    
+
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
+$("#files").change(function() {
+    filename = this.files[0].name;
+    console.log(filename);
+  });
+
+
+  function productUpdate() {
+    if ($("#productname").val() != null && $("#productname").val() != '') {
+        // Add product to Table
+        productAddToTable();
+
+        // Clear form fields
+        formClear();
+
+        // Focus to product name field
+        $("#productname").focus();
+    }
+}
